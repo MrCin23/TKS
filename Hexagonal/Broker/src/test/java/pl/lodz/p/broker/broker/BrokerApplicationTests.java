372d0b0a6e2f134fbea76b2712429bea1c30e3d7
MrCin23 <marcin.targonski23@gmail.com>
2025-05-23 14:11:06 +0200

    Broker start

---
+++ Hexagonal/Broker/src/test/java/pl/lodz/p/broker/broker/BrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.broker.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
